5b00e137d275aa3f6c6b68b5d0f81925375039da
hwchen <walther.chen@gmail.com>
2014-10-21 22:32:47 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Automata-rs
+
+Automata simulator written in Rust (0.13 nightly, 10/19/2014).
+
+Currently supports original Conway's Game of Life.
+
+In the future, may be refactored to include:
+
+- Langton's Ant.
+- Conway Variations.
+- prompt different grid sizes.
+
+dependency: 
+
+ncurses-rs by jeaye
+https://github.com/jeaye/ncurses-rs.git
+
+installation:
+
+    $ git clone https://github.com/hwchen/automata-rs.git
+    $ cd automata-rs
+    $ cargo build
+
+run:
+
+    $ ./target/automata
---
+++ src/main.rs
@@ -0,0 +1,180 @@
+// Conway's game of life
+//got to figure out "map" in rust.
+extern crate ncurses;
+
+use ncurses as n;
+use std::rand;
+
+struct Game {
+    grid: Vec<Vec<bool>> 
+}
+
+impl Game {
+    
+    fn new(x: uint, y: uint) -> Game {
+        let mut new_grid = vec![];
+        for _ in range(0,x) {
+            let mut row: Vec<bool> = vec![];
+            for _ in range(0,y) {
+                //add random
+                let r = rand::random::<uint>() % 2u;
+                match r {
+                    0 => row.push(false),
+                    1 => row.push(true),
+                    _ => row.push(false)
+                }
+            }
+            new_grid.push(row)
+        }
+        Game {grid: new_grid}
+    }
+
+    fn string(&self) -> String {
+        //map! (there's got to be an easier way
+        //to map on two vectors?
+        let mut print_grid = vec![];
+        for i in range(0, self.grid.len()) {
+            let mut row: Vec<&str> = vec![];
+            for j in range (0, self.grid.get(i).len()) {
+                if *self.grid.get(i).get(j) == false {
+                    row.push("   ");     
+                } else {
+                    row.push(" * ");
+                }
+            }
+            print_grid.push(row);
+        }
+
+        //collect to strings
+        let mut string_grid = vec![];
+        for i in range(0, print_grid.len()) {
+            let row = print_grid.get(i);
+            let mut string_row = row.concat();
+            string_row.push_str("\n");
+            string_grid.push(string_row);
+        }
+        string_grid.concat()
+    }
+
+    //since I have to build the whole grid on each refresh, I
+    //can't have a separate add_alive().
+    fn refresh_grid(&self, alives: Vec<(uint, uint)>) -> Game {
+        //initialize a new grid to false (could just call self.new())
+        let mut new_grid = vec![];
+        for i in range(0,self.grid.len()) {
+            let mut row: Vec<bool> = vec![];
+            for _ in range(0,self.grid.get(i).len()) {
+                row.push(false);
+            }
+            new_grid.push(row)
+        }
+        //add all the alives (don't need to worry about those marked
+        //for dead
+        for xy in alives.iter(){
+            match *xy {
+                (x,y) => *new_grid.get_mut(x).get_mut(y) = true
+            }
+        }
+        Game {grid: new_grid}
+    }
+
+    fn count_neighbors(&self, x: uint, y: uint) -> uint {
+        let mut sum = 0;
+        //top left
+        if (x > 0) && (y > 0) && (*self.grid.get(x-1).get(y-1) == true) {
+            sum += 1;
+        }
+        //top
+        if (x > 0) && (*self.grid.get(x-1).get(y) == true) {
+            sum += 1;
+        }
+        //top right
+        if (x > 0) && (y < self.grid.get(x).len()-1) && (*self.grid.get(x-1).get(y+1) == true) {
+            sum += 1;
+        }
+        //below left
+        if (x < self.grid.len()-1) && (y > 0) && (*self.grid.get(x+1).get(y-1) == true) {
+            sum += 1;
+        }
+        //below
+        if (x < self.grid.len()-1) && (*self.grid.get(x+1).get(y) == true) {
+            sum += 1;
+        }
+        //below right
+        if (x < self.grid.len()-1) && (y < self.grid.get(x).len()-1) && (*self.grid.get(x+1).get(y+1) == true) {
+            sum += 1;
+        }
+        //left
+        if (y > 0) && (*self.grid.get(x).get(y-1) == true) {
+            sum += 1;
+        }
+        //right
+        if (y < self.grid.get(x).len()-1) && (*self.grid.get(x).get(y+1) == true) {
+            sum += 1;
+        }
+        sum
+    }
+
+    fn calc_next_alive(&self) -> Vec<(uint, uint)> {
+        let mut res: Vec<(uint, uint)> = vec![];
+        for x in range(0, self.grid.len()){
+            for y in range(0, self.grid.get(x).len()) {
+                let neighbors = self.count_neighbors(x,y);
+                match neighbors {
+                    2 if *self.grid.get(x).get(y) == true => res.push((x,y)),
+                    3                                     => res.push((x,y)),
+                    _             => continue
+                }
+            }
+        }
+    res
+    }
+}
+
+
+fn main() {
+    
+    let mut game = Game::new(30, 30);
+    n::initscr();
+    loop{
+        n::printw("Conway's Game of Life\n");
+        n::printw("by Walther Chen\n\n\n");
+        n::printw(game.string().as_slice());
+        n::refresh();
+        
+        let alive = game.calc_next_alive();
+        game = game.refresh_grid(alive);
+        n::napms(1000);
+        n::clear();
+    }
+    n::endwin();
+
+    
+
+
+//    tests for neighbor count
+//    let neighbors = game.count_neighbors(1,1);
+//    println!("(1,1) has {} neighbors", neighbors);
+
+//    let neighbors = game.count_neighbors(0,0);
+//    println!("(0,0) has {} neighbors", neighbors);
+    
+//    let neighbors = game.count_neighbors(0,1);
+//    println!("(0,1) has {} neighbors", neighbors);
+
+//    let neighbors = game.count_neighbors(1,4);
+//    println!("(1,4) has {} neighbors", neighbors);
+
+//    let neighbors = game.count_neighbors(19,0);
+//    println!("(19,0) has {} neighbors", neighbors);
+    
+//    let neighbors = game.count_neighbors(0,9);
+//    println!("(0,9) has {} neighbors", neighbors);
+
+//    let neighbors = game.count_neighbors(19,9);
+//    println!("(19,9) has {} neighbors", neighbors);
+}
+
+
+
+
